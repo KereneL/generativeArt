4fd12132025cad8c1bfe171ce696f32f7a61f9c8
Kerem Yael Landman <40403339+KereneL@users.noreply.github.com>
2021-06-24 10:26:58 +0300

    upd

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+const canvasWidth = 500;
+const canvasHeight = 500;
+
 function setup() {
-  createCanvas(500, 500);
+    createCanvas(canvasWidth, canvasHeight);
+    background(0);
+    stroke('white');
+
+    for (let i = 0; i<500; i++){
+        randomPoint()
+    }
+  }
+function draw() {
+
 }
 
-function draw() {
-  background(100);
+function randomPoint(){
+    var rndX = random(canvasWidth);
+    var rndY = random(canvasHeight);
+    var rndW = Math.pow(random(1.5),2);
+    strokeWeight(rndW);
+    point(rndX,rndY);
 }
